74505eeb878e64dc77714def35e440546904fe7f
MichaelAstreiko <michael.astreiko@gmail.com>
2012-11-09 18:30:55 +0300

    Project Initialization

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,91 @@
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.work.dir = "target"
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
+        // excludes 'ehcache'
+    }
+    log "info" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+
+    repositories {
+        inherits true // Whether to inherit repository definitions from plugins
+
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+
+        mavenLocal()
+        mavenCentral()
+
+        mavenRepo "http://maven.springframework.org/release"
+        mavenRepo "http://maven.springframework.org/snapshot"
+        mavenRepo "http://maven.springframework.org/milestone"
+        //Spring Social Google <id>spring.social.google</id>
+        mavenRepo "https://github.com/GabiAxel/maven/raw/master"
+        mavenRepo "https://artifacts.alfresco.com/nexus"
+
+        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+        runtime 'mysql:mysql-connector-java:5.1.20'
+        compile("org.codehaus.groovy.modules.http-builder:http-builder:0.5.2") {
+            excludes "antlr", "appengine-api", "commons-collections", "commons-logging", "groovy", "xercesImpl", "httpclient"
+        }
+        compile "org.codehaus.jackson:jackson-mapper-asl:1.9.9"
+        compile "org.springframework.data:spring-data-jpa:1.1.0.RELEASE"
+        compile "org.springframework:spring-oxm:3.1.2.RELEASE"
+        compile "com.google.api-client:google-api-client:1.10.3-beta"
+        compile("org.springframework.social:spring-social-core:1.1.0.BUILD-SNAPSHOT",
+                "org.springframework.social:spring-social-web:1.1.0.BUILD-SNAPSHOT",
+                "org.springframework.social:spring-social-facebook:1.1.0.BUILD-SNAPSHOT",
+                "org.springframework.social:spring-social-twitter:1.0.2.RELEASE",
+                "org.springframework.social:spring-social-google:1.0.0.M1"
+        ) { transitive = false }
+        // Workaround to resolve dependency issue with aws-java-sdk and http-builder (dependent on httpcore:4.0)
+        build 'org.apache.httpcomponents:httpcore:4.1'
+        build 'org.apache.httpcomponents:httpclient:4.1.1'
+        runtime 'org.apache.httpcomponents:httpcore:4.1'
+        runtime 'org.apache.httpcomponents:httpclient:4.1.1'
+    }
+
+    plugins {
+        runtime ":hibernate:$grailsVersion"
+        runtime ":jquery:1.7.2"
+        runtime ":jquery-ui:1.8.24"
+        runtime ":resources:1.1.6"
+
+        // Uncomment these (or add new ones) to enable additional resources capabilities
+        //runtime ":zipped-resources:1.0"
+        //runtime ":cached-resources:1.0"
+        //runtime ":yui-minify-resources:0.1.4"
+
+        build ":tomcat:$grailsVersion"
+
+        runtime ":database-migration:1.0"
+
+        compile ':cache:1.0.0'
+        runtime ":spring-security-core:1.2.7.3"
+        compile(":spring-social-core:0.1.31") {
+            excludes "spring-security-core", "spring-social-core", "spring-social-web", "jackson-mapper-asl"
+        }
+
+        compile(":spring-social-twitter:0.1.31") { // keept to avoid compilation problem and right spring loading order
+            excludes "spring-security-core", "spring-social-twitter"
+        }
+    }
+}
---
+++ grails-app/conf/Config.groovy
@@ -0,0 +1,125 @@
+// locations to search for config files that get merged into the main config;
+// config files can be ConfigSlurper scripts, Java properties files, or classes
+// in the classpath in ConfigSlurper format
+
+// grails.config.locations = [ "classpath:${appName}-config.properties",
+//                             "classpath:${appName}-config.groovy",
+//                             "file:${userHome}/.grails/${appName}-config.properties",
+//                             "file:${userHome}/.grails/${appName}-config.groovy"]
+
+// if (System.properties["${appName}.config.location"]) {
+//    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
+// }
+
+grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
+grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
+grails.mime.use.accept.header = false
+grails.mime.types = [
+    all:           '*/*',
+    atom:          'application/atom+xml',
+    css:           'text/css',
+    csv:           'text/csv',
+    form:          'application/x-www-form-urlencoded',
+    html:          ['text/html','application/xhtml+xml'],
+    js:            'text/javascript',
+    json:          ['application/json', 'text/json'],
+    multipartForm: 'multipart/form-data',
+    rss:           'application/rss+xml',
+    text:          'text/plain',
+    xml:           ['text/xml', 'application/xml']
+]
+
+// URL Mapping Cache Max Size, defaults to 5000
+//grails.urlmapping.cache.maxsize = 1000
+
+// What URL patterns should be processed by the resources plugin
+//grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
+grails.resources.adhoc.patterns = []
+
+// The default codec used to encode data with ${}
+grails.views.default.codec = "none" // none, html, base64
+grails.views.gsp.encoding = "UTF-8"
+grails.converters.encoding = "UTF-8"
+// enable Sitemesh preprocessing of GSP pages
+grails.views.gsp.sitemesh.preprocess = true
+// scaffolding templates configuration
+grails.scaffolding.templates.domainSuffix = 'Instance'
+
+// Set to false to use the new Grails 1.2 JSONBuilder in the render method
+grails.json.legacy.builder = false
+// enabled native2ascii conversion of i18n properties files
+grails.enable.native2ascii = true
+// packages to include in Spring bean scanning
+grails.spring.bean.packages = []
+// whether to disable processing of multi part requests
+grails.web.disable.multipart=false
+
+// request parameters to mask when logging exceptions
+grails.exceptionresolver.params.exclude = ['password']
+
+// configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
+grails.hibernate.cache.queries = false
+
+basePath = System.properties['base.dir']
+
+environments {
+    development {
+        grails.logging.jul.usebridge = true
+        grails.plugins.springsocial.facebook.clientId = "478732525471377"
+        grails.plugins.springsocial.facebook.clientSecret = "cd3e5f402a4923f02bf3e0f28367f85f"
+        grails.serverURL = "http://localhost:8080/walkingme"
+    }
+    test {
+        grails.plugins.springsocial.facebook.clientId = "166470550161909"
+        grails.plugins.springsocial.facebook.clientSecret = "6653351a81af90272ef716c3b4af249b"
+        basePath = System.properties['catalina.base'] + "/webapps/ROOT"
+        grails.serverURL = "http://walkingme.realizeideas.net"
+    }
+    production {
+        grails.logging.jul.usebridge = false
+        grails.serverURL = "http://www.walkingme.com"
+        grails.plugins.springsocial.facebook.clientId = "485051521522395"
+        grails.plugins.springsocial.facebook.clientSecret = "05a62c08c926da9d6534de193fb311ab"
+        basePath = System.properties['catalina.base'] + "/webapps/ROOT"
+    }
+
+}
+
+// log4j configuration
+log4j = {
+    // Example of changing the log pattern for the default console appender:
+    //
+    appenders {
+        rollingFile name: 'file', maxFileSize: 5120000, file: "${basePath}/logs/walkingme.log", maxBackupIndex: 10, layout: pattern(conversionPattern: '%d [%t] %-5p (%c) - %m%n'), encoding: 'UTF-8'
+        console name: 'stdout', layout: pattern(conversionPattern: '%d [%t] %-5p (%c) - %m%n'), encoding: 'UTF-8'
+    }
+
+        info 'org.codehaus.groovy.grails.web.servlet'        // controllers
+//    debug 'org.hibernate.SQL'              // SQL logging
+//    debug 'org.apache.http'              // HTTP logging
+    error 'org.codehaus.groovy.grails.web.pages',          // GSP
+            'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+            'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+            'org.codehaus.groovy.grails.commons',            // core / classloading
+            'org.codehaus.groovy.grails.plugins',            // plugins
+            'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+            'org.springframework',
+            'org.hibernate',
+            'net.sf.ehcache.hibernate'
+
+}
+
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'net.realizeideas.walkingme.authentication.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'net.realizeideas.walkingme.authentication.UserRole'
+grails.plugins.springsecurity.authority.className = 'net.realizeideas.walkingme.authentication.Role'
+grails.plugins.springsecurity.requestMap.className = 'net.realizeideas.walkingme.authentication.Requestmap'
+grails.plugins.springsecurity.securityConfigType = grails.plugins.springsecurity.SecurityConfigType.Requestmap
+grails.plugins.springsecurity.rememberMe.alwaysRemember = true
+
+
+grails.plugin.databasemigration.updateOnStart = true
+grails.plugin.databasemigration.updateOnStartFileNames = ['changelog.groovy']
+
